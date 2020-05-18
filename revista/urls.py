a209d2881bdd6da605a4f431c85b0211b2955fc6
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-05-18 15:15:31 -0500

    Add comment form and article template

--- revista/urls.py
+++ revista/urls.py
@@ -6,5 +6,5 @@ app_name = "revista"
 
 urlpatterns = [
     path('', views.index.as_view(), name='home'),
-    #path('<slug:slug>/', views.post_detail, name = 'post_detail'),
+    path('<slug:slug>/', views.article_detail, name = 'article_detail'),
 ]
--- revista/views.py
+++ revista/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
-from .models import Article
+from .models import Article, Comment
+from .forms import CommentForm
 
 
 # def index(request):
@@ -10,3 +11,27 @@ from .models import Article
 class index(generic.ListView):
     queryset = Article.objects.all()
     template_name = 'revista/index.html'
+
+def article_detail(request, slug):
+    template_name = 'revista/article.html'
+    article = get_object_or_404(Article, slug=slug)
+    comments = article.comments.filter(active = True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data = request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit = False)
+            # Assign the current post to the comment
+            new_comment.article = article
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, { 'article': article,
+                                            'comments': comments,
+                                            'new_comment': new_comment,
+                                            'comment_form': comment_form})
