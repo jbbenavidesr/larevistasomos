c4f8811f2b839b975bcb1f1eb1b939289ec09c46
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-05-18 19:20:21 -0500

    Creo que soy un idiota si era esa coma

--- larevistasomos/settings.py
+++ larevistasomos/settings.py
@@ -176,7 +176,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
