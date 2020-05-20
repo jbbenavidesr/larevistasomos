4f4925f6b79c12608fb21cc5ef11a1b077f8d6c1
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-05-20 12:41:38 -0500

    Check when copypasting

--- larevistasomos/settings.py
+++ larevistasomos/settings.py
@@ -179,7 +179,7 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'larevistasomos.storage_backends.MediaStorage'
 
 
 django_heroku.settings(locals(), staticfiles=False)
