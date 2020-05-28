47b69280c4e0401a4d873fa82d4ed6997009c0b6
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-05-28 17:42:08 -0500

    Contact and success ready to go

--- contact/urls.py
+++ contact/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import contactView, successView
+from .views import contactView, SuccessView
 
 app_name = "contact"
 
 urlpatterns = [
     path('', contactView, name='contact'),
-    path('success/', successView, name='success'),
+    path('success/', SuccessView.as_view(), name='success'),
 ]
