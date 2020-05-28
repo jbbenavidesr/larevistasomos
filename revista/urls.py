ad65e89a7165aae2c9b2b66dec876600a7a37e48
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-05-27 22:00:31 -0500

    Archive view

--- revista/urls.py
+++ revista/urls.py
@@ -6,6 +6,7 @@ app_name = "revista"
 
 urlpatterns = [
     path('', views.Index.as_view(), name='home'),
+    path('archive/', views.ArchiveIndex.as_view(), name='archive_index'),
     path('quien-somos/', views.AboutUs.as_view(), name = 'about_us'),
     path('<slug:category>/', views.CategoryList.as_view(), name='category'),
     path('<slug:category>/<slug:slug>/', views.article_detail, name = 'article_detail'),
--- revista/views.py
+++ revista/views.py
@@ -50,6 +50,44 @@ class Index(generic.ListView):
         )
         return context
 
+class ArchiveIndex(generic.ListView):
+    template_name = 'revista/index.html'
+    enddate = datetime.date(2020, 5, 1)
+
+    def get_queryset(self):
+        """
+        Return the articles in this edition (not including those set to be
+        published in the future ). 
+        """
+        
+        return Article.objects.filter(
+            pub_date__lte = self.enddate   
+        ).exclude(
+            category__slug = 'cuentos'
+        ).exclude(
+            category__slug = 'poemas'
+        ).order_by('-pub_date')
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['archive_post'] = Article.objects.filter(
+            pub_date__lte = datetime.date(2020, 5, 1)
+        ).order_by('-pub_date')[:3]
+
+        context['cuentos'] = Article.objects.filter(
+            category__slug = 'cuentos',
+            pub_date__lte = self.enddate  
+        )
+
+        context['poemas'] = Article.objects.filter(
+            category__slug = 'poemas',
+            pub_date__lte = self.enddate  
+        )
+        return context
+
+
 class CategoryList(generic.ListView):
     template_name = 'revista/index.html'
 
