5851a9d37493bd13adb5b782721409e0ab893bed
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-09-07 19:15:39 -0500

    Deployment updates

--- larevistasomos/urls.py
+++ larevistasomos/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('cms/', admin.site.urls),
     path('contact/', include('contact.urls')),
     path('galeria/', include('gallery.urls')),
     path('', include('revista.urls')),
